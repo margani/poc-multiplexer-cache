f8b6f61228ee0b9fd797991d6366e32d7ecd6181
Hossein Margani <hossein.margani@aqovia.com>
2023-05-21 16:00:16 +0100

    Initial PoC

---
+++ MultiplexerPoCProgram.cs
@@ -0,0 +1,48 @@
+﻿namespace PoCMultiplexer;
+
+using System;
+using Shared;
+using StackExchange.Redis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new Config();
+        var multiplexerCache = new MultiplexerCache(config.PrimaryRedisConnectionString, config.SecondaryRedisConnectionString);
+
+        multiplexerCache.TestPutGet();
+    }
+}
+
+class MultiplexerCache
+{
+    private const int DefaultDatabaseNumber = 0;
+    private ConnectionMultiplexer ConnectionMultiplexer { get; }
+
+    private IDatabase Database { get; }
+
+    public MultiplexerCache(string primaryConnectionString, string secondaryConnectionString)
+    {
+        ConnectionMultiplexer = ConnectionMultiplexer.Connect($"{primaryConnectionString},{secondaryConnectionString}");
+        Database = ConnectionMultiplexer.GetDatabase(DefaultDatabaseNumber);
+    }
+
+    private void Put(string key, string value)
+    {
+        Database.StringSet(key, value);
+    }
+
+    private string Get(string key)
+    {
+        return Database.StringGet(key).ToString();
+    }
+
+    public void TestPutGet()
+    {
+        var key = "key for multiplexer cache poc";
+        Put(key, "example value 2");
+        var value = Get(key);
+        Console.WriteLine($"Returned value: {value}");
+    }
+}
